65adaa8de3322604e34266e08affabdf3c8aaf56
SaiVK <saiganesha5.svkv@gmail.com>
2024-06-07 13:03:01 +0530

    Fixes

--- build_crypto_unikernels.sh
+++ build_crypto_unikernels.sh
@@ -1,5 +1,6 @@
 # 
 eval $(opam env)
+opam reinstall mirage-logs-riscv -y
 cd unikernels
 
 # build aes-cbc
@@ -7,5 +8,7 @@ cd mirage-crypto-riscv-unikernel/cipher/aes-cbc-riscv-unikernel
 mirage config -t riscv
 cp ../../../../custom_makefile Makefile
 make build
+export OCAMLPARAM="insert-all-checkcap=1,function_sections=1,default-compartment-id=255,fides-cap-path=/home/fides/fides-crypto-demo/unikernels/mirage-crypto-riscv-unikernel/cipher/aes-cbc-riscv-unikernel,_"
 make kernel
 # spike kernel
+
